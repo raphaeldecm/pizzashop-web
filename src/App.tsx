6eaa81ca78a9dfe737352df9293ffd9176c6fdbd
Raphael Muniz <raphael.muniz@ifrn.edu.br>
2024-07-24 20:20:56 -0300

    feat: Add react-query and axios configurations
    This commit adds the necessary configurations for using react-query and axios in the project. It includes the creation of a `react-query.ts` file to initialize the `QueryClient` and a `axios.ts` file to create an instance of axios with the base URL from the environment variables. These configurations will enable the usage of react-query for data fetching and axios for making HTTP requests in the application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./global.css";
 
+import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { Toaster } from "sonner";
 
 import { ThemeProvider } from "./components/theme/theme-provider";
+import { queryClient } from "./lib/react-query";
 import { router } from "./routes";
 
 export function App() {
@@ -11,7 +13,9 @@ export function App() {
     <>
       <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
         <Toaster richColors />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   );
