a16c3cc869c3cb201f9e9da51f3dfa6bb3faee82
Raphael Muniz <raphael.muniz@ifrn.edu.br>
2024-07-26 14:18:21 -0300

    feat: Add OrderStatus type and API functions for order management

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -14,18 +14,29 @@ import {
 
 import { OrderTableFilters } from "./order-table-filter";
 import { OrderTableRow } from "./order-table-row";
+import { OrderTableSkeleton } from "./order-table-skeleton";
 
 export function Orders() {
   const [searchParams, setSearchParams] = useSearchParams();
+
+  const orderId = searchParams.get("orderId");
+  const customerName = searchParams.get("customerName");
+  const status = searchParams.get("status");
 
   const pageIndex = z.coerce
     .number()
     .transform((page) => page - 1)
     .parse(searchParams.get("page") ?? "1");
 
-  const { data: result } = useQuery({
-    queryKey: ["orders", pageIndex],
-    queryFn: () => getOrders({ pageIndex }),
+  const { data: result, isLoading: isLoadingOrders } = useQuery({
+    queryKey: ["orders", pageIndex, orderId, customerName, status],
+    queryFn: () =>
+      getOrders({
+        pageIndex,
+        orderId,
+        customerName,
+        status: status === "all" ? null : status,
+      }),
   });
 
   function handlePaginate(pageIndex: number) {
@@ -65,6 +76,7 @@ export function Orders() {
               </TableBody>
             </Table>
           </div>
+          {isLoadingOrders && <OrderTableSkeleton />}
 
           {result && (
             <Pagination
